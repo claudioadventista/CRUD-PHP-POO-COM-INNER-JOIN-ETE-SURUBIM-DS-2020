de5fa2f8ee289cd8834d7a73f977baf67614aa66
claudioadventista <claudioadventista@hotmail.com>
2020-11-04 22:30:39 -0300

    criado o arquivo Top.php na pasta html

---
+++ html/Top.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+		<link rel="stylesheet" type="text/css" href="../css/Style.css" />
+		
+		<link href="../fonts/fontawesome-free-5.11.2-web/css/fontawesome.css" rel="stylesheet">
+	  	<!--<link href="fonts/fontawesome-free-5.11.2-web/css/brands.css" rel="stylesheet">-->
+	  	<link href="../fonts/fontawesome-free-5.11.2-web/css/solid.css" rel="stylesheet">
+	</head>
